f02704a2a0c9d750f489ed17e9aaf9dde10c9e06
I.Zhuravlev <freeeg@gmail.com>
2023-06-22 12:02:10 +0800

    initial

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+  singleAttributePerLine: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
